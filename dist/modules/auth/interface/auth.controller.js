818858dbc7c28cf5ca459e5bdbda21ac7c94c1e1
chien.cq <chien.cq@geekup.vn>
2022-12-30 03:00:59 +0700

    fix

--- dist/modules/auth/interface/auth.controller.js
+++ dist/modules/auth/interface/auth.controller.js
@@ -16,6 +16,7 @@ exports.AuthController = void 0;
 const nestjs_swagger_api_exception_decorator_1 = require("@nanogiants/nestjs-swagger-api-exception-decorator");
 const common_1 = require("@nestjs/common");
 const swagger_1 = require("@nestjs/swagger");
+const jsonwebtoken_1 = require("jsonwebtoken");
 const exceptions_1 = require("../../../exceptions");
 const user_service_1 = require("../../user/app/user.service");
 const user_presenter_1 = require("../../user/interface/presenter/user.presenter");
@@ -29,6 +30,48 @@ let AuthController = class AuthController {
     constructor(userService, authService) {
         this.userService = userService;
         this.authService = authService;
+    }
+    getToken(res) {
+        const privateKey = `
+    -----BEGIN RSA PRIVATE KEY-----
+    MIIEpAIBAAKCAQEAh0jHFwkkWXZ45jCuE+hCRUWVrL2/VXhqGGa8BmbqPo18d1lo
+    rUfDuYkjQZvQaO4HfCURmpwhzPrgErUt7ztuv3dRbDsHRIlIbSgDWdE2AsDrjhsN
+    bpSiXZPsuDL4wuKYwY4U478p9ugtuqai8bNOEomLz41qcVil1nTa6yxCyzPTsOlg
+    rYFD2C/VWcjHin+895WD5aekGxYq+u1XowSien/XhVNHjW6ZbickMKx0994gIK3+
+    ulhCuU+kGd7MR0DL8qFrLmQtT2ttOPolP5uxy9fFSSVxCNQrTLApjIRH4nAOzgLv
+    wh5dOEBw3FBbVvQGczgb5ajDw4X4z/TV1XqLYQIDAQABAoIBAAMLwWBIVlk9jq+r
+    nErCU6YmwL3FvKpoanabzwtTmz3u7nTLNn0yyMJLVzfvobnQFKrv1HlzPxtT8XI4
+    YjUxBM+CqKqXumRZlS/wewFVYZp+pJsTqrnw9qYl2cpEqwdLxenTOK8PR3sJu5hY
+    jsIDggwurL8+wU0VpG+7fC+X9LXJn76/8tAZPxG88zmvKCYwokimBjaZwC2b8F99
+    CE6c9QwCjXIFBdc3j0gmYx/Zc5tNRoyVm0acHCF/nrTHfhOLS0+R2tm2xMMKuEl+
+    vKnPkpUSreJElW+ohI2F8XCyUUOsYUsSmbe5fklFTa5pEmOoJBYnnKNuYmRLF/Mg
+    Nhvy5AECgYEAvG+LM9AZ0tqcN/SQPMYHtK5qRSssRgej7Px8jbO583X5wsjaHz94
+    shvQo0ppid8Fsgnwg9v3ccjD1ybA+dxgD+rhi8Nn8vEcyOrgwGFE8wlE+/aaXwtr
+    4erKlwTxh9AW1cN/cg5l8B8PbjyE6Lh+JltgqxJbmXwkUli83OAQxWECgYEAt8p7
+    Ckb/NTmO+vBqVLqutX3NnX/FhR1TbbTPI7bAjisIZ18bYEePACuyDKlPCbfOqhOi
+    NikN5h6Y+OS5uEHN3BiMsfu5FFof9V1wNfDwBfFhyv2aLthoN63SibCSDWAUuHKN
+    VGtZFv04yEsbmpHKSrJeLCAfVuabeplTUmu5hgECgYAG2U8H7RJjNYrkBcGQU+pH
+    yOcX6H/Nc82jeXHVfKjPjoQrbj1vUVFDt/sXVEcNucttMytIv5/4xMFJeqEc88OS
+    EIluGQYYMGdJbjAJDgABjV6ygb4Ook2jgnMmlglpVEjgMCDbtmZCGf7nlM+H3dzH
+    7B5RWXN22qo/S/ZhWMuDQQKBgQCwCiSiKnDGVVRrbZ4bfBhdxJsOsdTKdNV1bks9
+    xQ1Y30UUZld9d+0z16aaVA1dnI45/8ZOpDIo6cPSGpr9LyJ0H5ub2yViphSYW6Bg
+    OmBdAOCPoytslWH1euv/cH3ki9Qs+6P9aTkTOQaLas2M1vnxZDmpgDckcIwZ+QSD
+    HKW+AQKBgQCePhbvYAcuQ8xAHo5XZ9PRanTzD9OLO3Kl6Z8J1GasCQtRrLVLJghe
+    iJCFr6/gVKWx5/Wjeau3k22LMXrLNAYML8q4on1BnWx8C2uJK62Tu9Zh4cTK4xjZ
+    uJkr2sINb10vlE57J9/RZFSkPae5QqpHSGskTWnhdqgqQmmhZNGuQg==
+    -----END RSA PRIVATE KEY-----
+    `;
+        const payload = {
+            sub: '123',
+            name: 'chien.cq',
+            exp: Math.floor(Date.now() / 1000) + 60 * 1000000,
+        };
+        const token = (0, jsonwebtoken_1.sign)(payload, privateKey, { algorithm: 'HS256' });
+        res.set('content-type', 'application/json');
+        res.status(200);
+        res.send(JSON.stringify({
+            token,
+        }));
     }
     async userLogin(userLoginDto) {
         const user = await this.authService.validateUser(userLoginDto);
@@ -52,6 +95,13 @@ let AuthController = class AuthController {
         return new response_presenter_1.AuthResponsePresenter(userPresenter);
     }
 };
+__decorate([
+    (0, common_1.Post)('jwt'),
+    __param(0, (0, common_1.Res)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", void 0)
+], AuthController.prototype, "getToken", null);
 __decorate([
     (0, common_1.Post)('login'),
     (0, common_1.HttpCode)(common_1.HttpStatus.OK),
